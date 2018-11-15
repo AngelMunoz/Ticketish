6caee7d97db47abece1273d9baef2c712b558eec
Angel D. Munoz <scytes.lol@gmail.com>
2018-11-15 12:55:41 -0600

    feature(UserService): add a small user service, basic interface

---
+++ Ticketish/Services/IService.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Ticketish.Services
+{
+  public interface IService<T>
+  {
+    Task<List<T>> FindAsync();
+    Task<T> FindOneAsync(long id);
+    Task<T> CreateAsync(T payload);
+    Task UpdateAsync(long id, T payload);
+    Task<T> DestroyAsync(long id);
+  }
+}
---
+++ Ticketish/Services/UserService.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Ticketish.Models;
+
+namespace Ticketish.Services
+{
+  public class UserService : IService<User>
+  {
+    private readonly TckContext _context;
+
+    public UserService(TckContext context)
+    {
+      _context = context;
+    }
+
+    public async Task<User> CreateAsync(User payload)
+    {
+      var user = _context.Add(payload);
+      await _context.SaveChangesAsync();
+      return user.Entity;
+    }
+
+    public async Task<long> Count()
+    {
+      return await _context.Users.CountAsync();
+    }
+
+    public Task<User> DestroyAsync(long id)
+    {
+      throw new NotImplementedException();
+    }
+
+    public Task<List<User>> FindAsync()
+    {
+      throw new NotImplementedException();
+    }
+
+    public async Task<User> FindOneAsync(long id)
+    {
+      var user = await _context.Users.FindAsync(id);
+      if (user == null) { throw new ArgumentException(); }
+      return user;
+    }
+
+    public async Task<User> FindOneAsync(string email)
+    {
+      var user = await _context.Users.FirstOrDefaultAsync(u => u.Email == email);
+      if (user == null) { throw new ArgumentException(); }
+      return user;
+    }
+
+    public Task UpdateAsync(long id, User payload)
+    {
+      throw new NotImplementedException();
+    }
+  }
+}
